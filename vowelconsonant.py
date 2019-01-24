3a7a4672a2f9ac1e852992c594acf586af13f641
Arulmozhi936 <46783024+Arulmozhi936@users.noreply.github.com>
2019-01-24 20:53:03 +0530

    Update vowelconsonant.py

--- vowelconsonant.py
+++ vowelconsonant.py
@@ -1,8 +1,5 @@
 arul = input()
-if(arul=='!' or arul=='@ or arul=='~' or arul=='$' or arul =='%' or arul=='^'
- or arul=='&' or arul=='*' or arul=='(' or arul==')' or arul=='?'):
- print("Invalid")
-elif(arul=='A' or arul=='a' or arul=='E' or arul =='e' or arul=='I'
+if(arul=='A' or arul=='a' or arul=='E' or arul =='e' or arul=='I'
  or arul=='i' or arul=='O' or arul=='o' or arul=='U' or arul=='u'):
     print("Vowel")
 else:
