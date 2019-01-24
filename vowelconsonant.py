0394cb7350c0d821797b11328f23ec213a455a51
Arulmozhi936 <46783024+Arulmozhi936@users.noreply.github.com>
2019-01-24 20:22:38 +0530

    Update vowelconsonant.py

--- vowelconsonant.py
+++ vowelconsonant.py
@@ -1,7 +1,7 @@
 ar = input("Enter a character: ")
 
 if(ar=='A' or ar=='a' or ar=='E' or ar =='e' or ar=='I'
- or ar=='i' or ar=='O' or ch=='o' or ar=='U' or ar=='u'):
+ or ar=='i' or ar=='O' or ar=='o' or ar=='U' or ar=='u'):
     print("Vowel")
 else:
     print("Consonant")
