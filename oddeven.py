c0dbf6b9bd402802d435a3c171265e75c2fcefaa
mano1199 <46454509+mano1199@users.noreply.github.com>
2019-01-25 13:10:01 +0530

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -1,8 +1,7 @@
 num = int(input())
-if num < 0:
+if (num < 0):
       print("Negative")
-elif num == 0:
-   print("Zero")
-else:
-
-print("Positive")
+elif(num>0):
+      print('Positive')
+elif (num==0):
+	print('Zero')
